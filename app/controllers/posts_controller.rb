df731272d231ef0f5313e963b961c8a133845e7f
noahShafer <noahshafer0211@gmail.com>
2020-05-29 01:31:37 -0500

    added abilty to rate posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
-    before_action :get_post, only: [:show]
+    before_action :get_post, only: [:show, :classify]
+    before_action :get_session_user, only: [:show, :classify]
+
     def index
         @posts = Post.all.order(id: :desc)
     end
@@ -20,11 +22,11 @@ class PostsController < ApplicationController
                 name: quality.name, 
                 id: quality.id,
                 rating: @post.quality_rating(quality.id), 
-                user_count: @post.classifications.where(quality_id: quality.id).count,
-                classifiers_average: @post.classifiers_quality_average_raw(quality.id)
+                user_count: @post.classifications.where(quality_id: quality.id, active: true).count,
+                classifiers_average: @post.classifiers_quality_average_raw(quality.id),
+                is_classified: !@session_user.classifications_made.where(post_id: @post.id, quality_id: quality.id, active: true).empty?,
             }
         end
-        @session_user = User.find(session[:current_user_id])
         # if params[:sort_by_rating]
         #     @user_ratings.sort_by! { |rating| rating[:rating_raw] }.reverse!
         #     puts @user_ratings
@@ -33,9 +35,22 @@ class PostsController < ApplicationController
         # end
     end
 
+    def classify
+        if params[:is_classified] == "true"
+            @session_user.declassify_post(@post, params[:quality_id])
+        else
+            @session_user.classify_post(@post, params[:quality_id])
+        end
+        redirect_to post_path(@post)
+    end
+
     private
     def get_post
         @post = Post.find(params[:id])
+    end
+
+    def get_session_user
+        @session_user = User.find(session[:current_user_id])
     end
 
     def post_params
