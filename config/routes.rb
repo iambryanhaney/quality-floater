d8a3843217f4e73b7b01aa991af27e81a64d426f
noahShafer <noahshafer0211@gmail.com>
2020-05-29 13:01:55 -0500

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
+  root to: 'sessions#welcome'
 
   post 'posts/:id/classify', to: 'posts#classify', as: "classify"
 end
